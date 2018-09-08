eb3179b1767076e224c4beaaeb0237f8d6baf683
Takafumi Tamura <taktamur@gmail.com>
2018-09-08 17:35:22 +0900

    update

--- Twitter2/src/main/kotlin/main.kt
+++ Twitter2/src/main/kotlin/main.kt
@@ -1,12 +1,18 @@
-import twitter4j.*
-import twitter4j.auth.AccessToken
+import com.fasterxml.jackson.databind.ObjectMapper
+import twitter4j.Status
+import twitter4j.TwitterFactory
+import twitter4j.TwitterObjectFactory
 
 fun main(args: Array<String>) {
 
-    val twitter =TwitterFactory.getSingleton()
+    val twitter = TwitterFactory.getSingleton()
     val statues =twitter.getHomeTimeline()
     statues.forEach{
-        println("${it.user.name} : ${it.text}")
+        //println("${it.user.name} : ${it.text}")
+        val mapper = ObjectMapper()
+        val json = mapper.writeValueAsString(it)
+
+        println(json)
 
     }
 }
